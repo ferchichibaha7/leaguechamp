4b6dfb51470ce73881da1c593ee0ea8b14f4efbc
Baha <ferchichibaha7@gmail.com>
2020-02-19 13:56:17 +0100

    Updates

---
+++ precache-manifest.85e21a7d6f90f6af5bab4a3800ea47c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6e60e28107a8bf80ea0c64e4f1cfd8",
+    "url": "/leaguechamp/index.html"
+  },
+  {
+    "revision": "91e5ad851f610c80fccb",
+    "url": "/leaguechamp/static/css/main.1ce4651e.chunk.css"
+  },
+  {
+    "revision": "07443f0e253fe6570439",
+    "url": "/leaguechamp/static/js/2.4e3c5ed1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/leaguechamp/static/js/2.4e3c5ed1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e5ad851f610c80fccb",
+    "url": "/leaguechamp/static/js/main.fc03ab4c.chunk.js"
+  },
+  {
+    "revision": "32167f15575f442213ad",
+    "url": "/leaguechamp/static/js/runtime-main.579f45d0.js"
+  },
+  {
+    "revision": "8c0285f18d1e0a2ff4c79048f718953f",
+    "url": "/leaguechamp/static/media/bg.8c0285f1.jpg"
+  }
+]);
